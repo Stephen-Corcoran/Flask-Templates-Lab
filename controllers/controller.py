3ed90322986f1cf859df6db4e47392592391b793
Stephen <stephendcorcoran@gmail.com>
2021-07-01 15:03:45 +0100

    First commit

---
+++ controllers/controller.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from app import app
+from models.event_list import *
+
+@app.route('/')
+def index():
+    return render_template(index.html, title="Events list", events=events)
---
+++ models/event_list.py
@@ -0,0 +1,7 @@
+from models.event import *
+
+
+
+event1 = Event ("2021/07/01", "wedding", 20, "Ball room", "Mr & Mrs Smith",)
+event2 = Event ("2021/07/02", "Birthday party", 10, "restaurant", "Childs party")
+events = [event1, event2]
