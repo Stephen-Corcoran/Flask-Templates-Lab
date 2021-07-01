602831a5915c80689f218cc12db4a8b3793af16e
Stephen <stephendcorcoran@gmail.com>
2021-07-01 15:17:28 +0100

    All events listed

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,4 +4,4 @@ from models.event_list import *
 
 @app.route('/')
 def index():
-    return render_template(index.html, title="Events list", events=events)
+    return render_template('index.html', title="Events list", events=events)
--- models/event_list.py
+++ models/event_list.py
@@ -2,6 +2,6 @@ from models.event import *
 
 
 
-event1 = Event ("2021/07/01", "wedding", 20, "Ball room", "Mr & Mrs Smith",)
+event1 = Event ("2021/07/01", "wedding", 20, "Ball room", "Mr & Mrs Smith")
 event2 = Event ("2021/07/02", "Birthday party", 10, "restaurant", "Childs party")
 events = [event1, event2]
